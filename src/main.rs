e4cd8c2506370f923218bd2d9aeae19a45b04013
Jerry Wang <jerrylwang123@gmail.com>
2025-11-28 10:55:56 -0500

    feat: 2.1 first ppm image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+const IMAGE_WIDTH: usize = 256;
+const IMAGE_HEIGHT: usize = 256;
+
 fn main() {
-    println!("Hello, world!");
+    println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255");
+
+    for j in 0..IMAGE_HEIGHT {
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let g = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let b = 0.0;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
