f260010f9d65edd1d034edfbcf4ec6749cae85f1
Jerry Wang <jerrylwang123@gmail.com>
2025-11-28 11:54:31 -0500

    refactor: 3.1 use color and vec3 for main

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
+use std::error::Error;
+use std::io::{BufWriter, Write, stdout};
+
+use raytracing::color::{Color, write_color};
+
 const IMAGE_WIDTH: usize = 256;
 const IMAGE_HEIGHT: usize = 256;
 
-fn main() {
-    println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255");
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut out = BufWriter::new(stdout());
+
+    writeln!(out, "P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255")?;
 
     for j in 0..IMAGE_HEIGHT {
         eprintln!("\rScanlines remaining: {} ", IMAGE_HEIGHT - j);
         for i in 0..IMAGE_WIDTH {
-            let r = i as f32 / (IMAGE_WIDTH - 1) as f32;
-            let g = j as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let b = 0.0;
-
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            println!("{ir} {ig} {ib}");
+            let pixel_color = Color::new(
+                i as f64 / (IMAGE_WIDTH - 1) as f64,
+                j as f64 / (IMAGE_HEIGHT - 1) as f64,
+                0.0,
+            );
+            write_color(&mut out, &pixel_color)?;
         }
     }
     eprintln!("\rDone.");
+    Ok(())
 }
