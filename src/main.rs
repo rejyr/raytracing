ab2d38b827b8fc34d43c0cfab8afb8f9c998188c
Jerry Wang <jerrylwang123@gmail.com>
2025-12-02 11:31:45 -0500

    feat: 5.2 first raytraced image

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,20 @@ use raytracing::color::{Color, write_color};
 use raytracing::ray::Ray;
 use raytracing::vec3::{Point3, Vec3};
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = *center - *r.origin();
+    let a = r.direction().dot(r.direction());
+    let b = -2.0 * r.direction().dot(&oc);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4. * a * c;
+    discriminant >= 0.
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Point3::new(0., 0., -1.), 0.5, r) {
+        return Color::new(1., 0., 0.);
+    };
+
     let unit_direction = r.direction().unit_vector();
     let a = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - a) * Color::new(1., 1., 1.) + a * Color::new(0.5, 0.7, 1.)
