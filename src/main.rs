a0377e205056d81de0cf48c7c60ba927e771d73d
Jerry Wang <jerrylwang123@gmail.com>
2025-12-02 12:04:37 -0500

    feat: 6.2 simplifying the ray-sphere intersection

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use raytracing::vec3::{Point3, Vec3};
 
 fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = *center - *r.origin();
-    let a = r.direction().dot(r.direction());
-    let b = -2.0 * r.direction().dot(&oc);
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let a = r.direction().length_squared();
+    let h = r.direction().dot(&oc);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = h * h - a * c;
 
     if discriminant < 0.0 {
         -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (h - discriminant.sqrt()) / a
     }
 }
 
