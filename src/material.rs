41f5296db10425f128e184f455c52da1bade73b7
Jerry Wang <jerrylwang123@gmail.com>
2025-12-02 16:19:24 -0500

    feat: 11.3 total internal reflection

--- src/material.rs
+++ src/material.rs
@@ -94,9 +94,16 @@ impl Material for Dielectric {
         };
 
         let unit_direction = r_in.direction().unit_vector();
-        let refracted = unit_direction.refract(&rec.normal, ri);
+        let cos_theta = (-unit_direction).dot(&rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+        let cannot_refract = ri * sin_theta > 1.0;
+        let direction = if cannot_refract {
+            unit_direction.reflect(&rec.normal)
+        } else {
+            unit_direction.refract(&rec.normal, ri)
+        };
 
-        *scattered = Ray::new(rec.p, refracted);
+        *scattered = Ray::new(rec.p, direction);
         true
     }
 }
