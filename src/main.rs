b31f22dafe3b9118d16baa6dc043824d5d649fe0
Jerry Wang <jerrylwang123@gmail.com>
2025-12-02 11:41:37 -0500

    chore: use consistent .0 floats

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,18 @@ fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
     let a = r.direction().dot(r.direction());
     let b = -2.0 * r.direction().dot(&oc);
     let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4. * a * c;
-    discriminant >= 0.
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0., 0., -1.), 0.5, r) {
-        return Color::new(1., 0., 0.);
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
     };
 
     let unit_direction = r.direction().unit_vector();
     let a = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - a) * Color::new(1., 1., 1.) + a * Color::new(0.5, 0.7, 1.)
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -35,16 +35,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let focal_length = 1.0;
     let viewport_height = 2.0;
     let viewport_width = viewport_height * (image_width / image_height) as f64;
-    let camera_center = Point3::new(0., 0., 0.);
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
 
-    let viewport_u = Vec3::new(viewport_width, 0., 0.);
-    let viewport_v = Vec3::new(0., -viewport_height, 0.);
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
     let pixel_delta_u = viewport_u / image_width as f64;
     let pixel_delta_v = viewport_v / image_height as f64;
 
     let viewport_upper_left =
-        camera_center - Vec3::new(0., 0., focal_length) - viewport_u / 2. - viewport_v / 2.;
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
     let pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     let mut out = BufWriter::new(stdout());
