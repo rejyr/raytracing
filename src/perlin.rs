ed0710b4018ecfdd68477183cd476d0df64321de
Jerry Wang <jerrylwang123@gmail.com>
2025-12-06 21:21:11 -0500

    feat: 5.2 trilinear interpolation

--- src/perlin.rs
+++ src/perlin.rs
@@ -32,11 +32,26 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()) as i32 & 255;
-        let j = (4.0 * p.y()) as i32 & 255;
-        let k = (4.0 * p.z()) as i32 & 255;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        self.randfloat[self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]]
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]]
+                }
+            }
+        }
+
+        Self::trilinear_interp(c, u, v, w)
     }
 
     fn perlin_generate_perm() -> [usize; Self::POINT_COUNT] {
@@ -50,5 +65,24 @@ impl Perlin {
             let target = random_i32_in_range(0, i as i32) as usize;
             p.swap(target, i);
         }
+    }
+
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    let fi = i as f64;
+                    let fj = j as f64;
+                    let fk = k as f64;
+                    accum += (fi * u + (1.0 - fi) * (1.0 - u))
+                        * (fj * v + (1.0 - fj) * (1.0 - v))
+                        * (fk * w + (1.0 - fk) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+
+        accum
     }
 }
